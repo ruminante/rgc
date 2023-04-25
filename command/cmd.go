2ac0d6c2d7f6b7f56bbac481c10f418f60a855df
Obdurat <omicron_rpl@hotmail.com>
2023-04-24 21:37:39 -0300

    feat: :sparkles: Added root command and main.go

---
+++ command/cmd.go
@@ -0,0 +1,36 @@
+package command
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ruminante/conventional-commit/executor"
+	"github.com/spf13/cobra"
+)
+
+var undo bool
+var add bool
+var m string
+
+var rootCmd = &cobra.Command{
+  Use:   "rgc [flags]",
+  Short: "rgc is a conventional commit tool",
+  Long: `rgc (Ruminante Conventional Commit) is a conventional commit tool to help you write better commit messages and follow the conventional commit standard.`,
+  Run: func(cmd *cobra.Command, args []string) {
+    // Do Stuff Here
+		executor.Executor(cmd)
+  },
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolVarP(&add, "all", "a", false, "Select all files for git add")
+	rootCmd.PersistentFlags().StringVarP(&m, "message", "m", "", "Commit message for the commit")
+	rootCmd.PersistentFlags().BoolVar(&undo, "undo", false, "Select all files for git add")
+}
+
+func Execute() {
+  if err := rootCmd.Execute(); err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/ruminante/conventional-commit/command"
+)
+
+func main() {
+	command.Execute()
+}
